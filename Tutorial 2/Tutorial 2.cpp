e6319324ffb85a28c0a60fe577d2c85cb3c26928
dariusrichardosn55 <darius.richardson@me.com>
2020-03-08 16:42:22 +0000

    Changes

---
+++ Tutorial 2/Tutorial 2.cpp
@@ -0,0 +1,136 @@
+#include <iostream>
+#include <vector>
+
+#include "Utils.h"
+#include "CImg.h"
+
+using namespace cimg_library;
+
+void print_help() {
+	std::cerr << "Application usage:" << std::endl;
+
+	std::cerr << "  -p : select platform " << std::endl;
+	std::cerr << "  -d : select device" << std::endl;
+	std::cerr << "  -l : list all platforms and devices" << std::endl;
+	std::cerr << "  -f : select test.pgm" << std::endl;
+	std::cerr << "  -h : print this message" << std::endl;
+}
+
+int main(int argc, char **argv) {
+	//Part 1 - handle command line options such as device selection, verbosity, etc.
+	int platform_id = 0;
+	int device_id = 0;
+	string image_filename = "test.pgm";
+	for (int i = 1; i < argc; i++) {
+		if ((strcmp(argv[i], "-p") == 0) && (i < (argc - 1))) { platform_id = atoi(argv[++i]); }
+		else if ((strcmp(argv[i], "-d") == 0) && (i < (argc - 1))) { device_id = atoi(argv[++i]); }
+		else if (strcmp(argv[i], "-l") == 0) { std::cout << ListPlatformsDevices() << std::endl; }
+		else if ((strcmp(argv[i], "-f") == 0) && (i < (argc - 1))) { image_filename = argv[++i]; }
+		else if (strcmp(argv[i], "-h") == 0) { print_help(); return 0; }
+	}
+
+	cimg::exception_mode(0);
+
+	//detect any potential exceptions
+	try {
+		CImg<unsigned char> image_input(image_filename.c_str());
+		CImgDisplay disp_input(image_input,"input");
+
+		//a 3x3 convolution mask implementing an averaging filter
+		std::vector<float> convolution_mask = { 1.f / 9, 1.f / 9, 1.f / 9,
+												1.f / 9, 1.f / 9, 1.f / 9,
+												1.f / 9, 1.f / 9, 1.f / 9 };
+
+		//Part 3 - host operations
+		//3.1 Select computing devices
+		cl::Context context = GetContext(platform_id, device_id);
+
+		//display the selected device
+		std::cout << "Runing on " << GetPlatformName(platform_id) << ", " << GetDeviceName(platform_id, device_id) << std::endl;
+
+		//create a queue to which we will push commands for the device
+		cl::CommandQueue queue(context);
+
+		//3.2 Load & build the device code
+		cl::Program::Sources sources;
+
+		AddSources(sources, "kernels/my_kernels.cl");
+
+		cl::Program program(context, sources);
+
+		//build and debug the kernel code
+		try { 
+			program.build();
+		}
+		catch (const cl::Error& err) {
+			std::cout << "Build Status: " << program.getBuildInfo<CL_PROGRAM_BUILD_STATUS>(context.getInfo<CL_CONTEXT_DEVICES>()[0]) << std::endl;
+			std::cout << "Build Options:\t" << program.getBuildInfo<CL_PROGRAM_BUILD_OPTIONS>(context.getInfo<CL_CONTEXT_DEVICES>()[0]) << std::endl;
+			std::cout << "Build Log:\t " << program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(context.getInfo<CL_CONTEXT_DEVICES>()[0]) << std::endl;
+			throw err;
+		}
+
+		typedef int mytype;
+		size_t local_size = 10;
+
+		// number of bins
+		vector<int> H(256);
+		
+	    std::vector<mytype> A(10, 1);
+        size_t input_size = A.size() * sizeof(mytype);//size in bytes
+
+		// the input elements
+	    size_t input_elements = A.size();
+
+	
+		// output (host)
+		std::vector<mytype> B(input_elements);
+		size_t output_size = B.size() * sizeof(mytype);//
+
+		size_t nr_groups = input_elements / local_size;
+
+		// The number of bins for H
+		
+
+		//device - buffers
+		cl::Buffer buffer_A(context, CL_MEM_READ_ONLY, input_size);
+		cl::Buffer buffer_B(context, CL_MEM_READ_WRITE, output_size);
+
+		//4.1 Copy images to device memory
+		queue.enqueueWriteBuffer(buffer_A, CL_TRUE, 0, input_size, &A[0]);
+		queue.enqueueFillBuffer(buffer_B, 0, 0, output_size); //zero B buffer on device memory
+
+
+		//device - buffers
+		cl::Buffer dev_image_input(context, CL_MEM_READ_ONLY, image_input.size());
+		cl::Buffer dev_image_output(context, CL_MEM_READ_WRITE, image_input.size());
+		//4.2 Setup and execute the kernel (i.e. device code)
+		cl::Kernel kernel = cl::Kernel(program, "identityND");
+		kernel.setArg(0, dev_image_input);
+		kernel.setArg(1, dev_image_output);
+//		kernel.setArg(2, dev_convolution_mask);
+
+		queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(image_input.width(), image_input.height(), image_input.spectrum()), cl::NullRange);
+
+		vector<unsigned char> output_buffer(image_input.size());
+		//4.3 Copy the result from device to host
+		queue.enqueueReadBuffer(dev_image_output, CL_TRUE, 0, output_buffer.size(), &output_buffer.data()[0]);
+
+		CImg<unsigned char> output_image(output_buffer.data(), image_input.width(), image_input.height(), image_input.depth(), image_input.spectrum());
+		CImgDisplay disp_output(output_image,"output");
+
+ 		while (!disp_input.is_closed() && !disp_output.is_closed()
+			&& !disp_input.is_keyESC() && !disp_output.is_keyESC()) {
+		    disp_input.wait(1);
+		    disp_output.wait(1);
+	    }		
+
+	}
+	catch (const cl::Error& err) {
+		std::cerr << "ERROR: " << err.what() << ", " << getErrorString(err.err()) << std::endl;
+	}
+	catch (CImgException& err) {
+		std::cerr << "ERROR: " << err.what() << std::endl;
+	}
+
+	return 0;
+}
